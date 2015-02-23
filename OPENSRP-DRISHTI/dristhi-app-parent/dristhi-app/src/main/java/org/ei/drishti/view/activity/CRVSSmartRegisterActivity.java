ccc1fa8edc583006ce246a58a6f6920c555494f3
Maimoona Kausar <maimoona.kausar@ihsinformatics.com>
2015-02-23 12:35:46 +0500

    updated views

--- OPENSRP-DRISHTI/dristhi-app-parent/dristhi-app/src/main/java/org/ei/drishti/view/activity/CRVSSmartRegisterActivity.java
+++ OPENSRP-DRISHTI/dristhi-app-parent/dristhi-app/src/main/java/org/ei/drishti/view/activity/CRVSSmartRegisterActivity.java
@@ -1,12 +1,12 @@
 package org.ei.drishti.view.activity;
 
-import org.ei.drishti.view.controller.ChildSmartRegisterController;
+import org.ei.drishti.view.controller.CRVSSmartRegisterController;
 
 public class CRVSSmartRegisterActivity extends SmartRegisterActivity {
     @Override
     protected void onSmartRegisterInitialization() {
-        webView.addJavascriptInterface(new ChildSmartRegisterController(context.serviceProvidedService(), context.alertService(),
-                context.allBeneficiaries(), context.listCache()), "context");
-        webView.loadUrl("file:///android_asset/www/smart_registry/child_register.html");
+        webView.addJavascriptInterface(new CRVSSmartRegisterController(context.serviceProvidedService(), context.alertService(),
+                context.formDataRepository(), context.listCache()), "context");
+        webView.loadUrl("file:///android_asset/www/smart_registry/crvs_register.html");
     }
 }
